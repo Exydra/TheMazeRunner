c8d5f9c76249d6bd6d0787eeb366d5d4c83869f2
jan-claeys <janclaeys01@gmail.com>
2018-02-09 15:34:31 +0100

    eerste route

--- app/src/main/java/com/example/jan_c/themazerunner/Kaart.java
+++ app/src/main/java/com/example/jan_c/themazerunner/Kaart.java
@@ -1,23 +1,46 @@
 package com.example.jan_c.themazerunner;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CustomCap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.tasks.Task;
 
 import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.graphics.Color.rgb;
+import static java.lang.StrictMath.toRadians;
 
 public class Kaart extends FragmentActivity implements OnMapReadyCallback {
-
+    private LatLng CurrentLocation = new LatLng(51.161891212585495, 4.136264966509771);
     private GoogleMap mMap;
-
+    private Timer timer;
+    private LatLng huidigeLocatie;
+    private Polyline polyline1a;
+    private Polyline polyline1b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +49,7 @@ public class Kaart extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -40,66 +64,145 @@ public class Kaart extends FragmentActivity implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        try {
+            mMap = googleMap;
 
-        mMap.addMarker(new MarkerOptions().position(new LatLng(51.161891212585495, 4.136264966509771))
-                .title("Start")
-        );
-        mMap.addMarker(new MarkerOptions().position(new LatLng(51.17039054011921,4.142726591135215))
-                .title("Station")
-        );
-        mMap.addMarker(new MarkerOptions().position(new LatLng(51.16623048764471,4.144357827015028))
-                .title("STEM")
-        );
-        mMap.addMarker(new MarkerOptions().position(new LatLng(51.16476175829673,4.14110284877097))
-                .title("Markt")
-        );
-        mMap.addMarker(new MarkerOptions().position(new LatLng(51.161891212585495, 4.136264966509771))
-                .title("Finish")
-        );
-        // Add a marker in Sydney and move the camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.161891212585495,4.136264966509771 ), 16));
-        Polyline polilyne1a = mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-                .add( new LatLng( 51.161891212585495,4.136264966509771 ),
-                      new LatLng( 51.16215866116135,4.136771904013585 ),
-                       new LatLng(51.16257917474246,4.137313710562012 ),
-                       new LatLng(51.16309313972857,4.138180934842239),
-                       new LatLng(51.163863502158144,4.139504604950162 ),
-                       new LatLng(51.16487679373538,4.14096956144931) ,
-                        new LatLng(51.16491716166276,4.141339705965947),
-                       new LatLng(51.16775212389541,4.142041974555468) ,
-                       new LatLng(51.17039054011921,4.14272659135215) )
-        );
-        Polyline polilyne1b = mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-        .add( new LatLng( 51.17039054011921,4.142726591135215 ),
-                new LatLng( 51.17047039826386,4.143024769073236 ),
-                new LatLng( 51.17026648447245,4.14324001651039),
-                new LatLng(51.16853341661673,4.144846877443342),
-                new LatLng( 51.16846690763632,4.144748323574277),
-                new LatLng(51.168352542886,4.144693338289471) ,
-                new LatLng(51.16823313230337,4.144772463414483),
-                new LatLng(51.16816249493602,4.144973629131528) ,
-                new LatLng(51.16623048764471,4.144357827015028) )
-        );
-        Polyline polilyne1c = mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-         .add( new LatLng(  51.16623048764471,4.144357827015028),
-                new LatLng( 51.165503423384166,4.1440817945931485),
-                new LatLng( 51.16525113164939,4.143206053431641),
-                new LatLng(51.16502817056048,4.142514513841888),
-                new LatLng(51.16492154495015,4.141339218440407 ),
-                new LatLng(51.16476175829673,4.14110284877097))
-        );
-        Polyline polilyne1d = mMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-        .add( new LatLng( 51.16476175829673,4.14110284877097 ),
-                new LatLng(51.163863502158144,4.139504604950162 ),
-                new LatLng(51.16309313972857,4.138180934842239 ),
-                new LatLng(51.16257917474246,4.137313710562012),
-                new LatLng(51.16215866116135,4.136771904013585  ),
-                new LatLng(51.161891212585495,4.136264966509771 ))
-        );
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.161891212585495, 4.136264966509771))
+                    .title("Start")
+            );
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.17039054011921, 4.142726591135215))
+                    .title("Station")
+            );
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.16623048764471, 4.144357827015028))
+                    .title("STEM")
+                    .visible(false)
+            );
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.16476175829673, 4.14110284877097))
+                    .title("Markt")
+                    .visible(false)
+            );
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.161891212585495, 4.136264966509771))
+                    .title("Finish")
+                    .visible(false)
+            );
+            // Add a marker in Sydney and move the camera
+             polyline1a = mMap.addPolyline(new PolylineOptions()
+                    .clickable(true)
+                    .add(new LatLng(51.161891212585495, 4.136264966509771),
+                            new LatLng(51.16215866116135, 4.136771904013585),
+                            new LatLng(51.16257917474246, 4.137313710562012),
+                            new LatLng(51.16309313972857, 4.138180934842239),
+                            new LatLng(51.163863502158144, 4.139504604950162),
+                            new LatLng(51.16487679373538, 4.14096956144931),
+                            new LatLng(51.16491716166276, 4.141339705965947),
+                            new LatLng(51.16775212389541, 4.142041974555468),
+                            new LatLng(51.17039054011921, 4.14272659135215))
+            );
+            polyline1b = mMap.addPolyline(new PolylineOptions()
+                    .clickable(true)
+                    .add(new LatLng(51.17039054011921, 4.142726591135215),
+                            new LatLng(51.17047039826386, 4.143024769073236),
+                            new LatLng(51.17026648447245, 4.14324001651039),
+                            new LatLng(51.16853341661673, 4.144846877443342),
+                            new LatLng(51.16846690763632, 4.144748323574277),
+                            new LatLng(51.168352542886, 4.144693338289471),
+                            new LatLng(51.16823313230337, 4.144772463414483),
+                            new LatLng(51.16816249493602, 4.144973629131528),
+                            new LatLng(51.16623048764471, 4.144357827015028))
+                    .visible(false)
+            );
+            Polyline polilyne1c = mMap.addPolyline(new PolylineOptions()
+                    .clickable(true)
+                    .add(new LatLng(51.16623048764471, 4.144357827015028),
+                            new LatLng(51.165503423384166, 4.1440817945931485),
+                            new LatLng(51.16525113164939, 4.143206053431641),
+                            new LatLng(51.16502817056048, 4.142514513841888),
+                            new LatLng(51.16492154495015, 4.141339218440407),
+                            new LatLng(51.16476175829673, 4.14110284877097))
+                    .visible(false)
+            );
+            Polyline polilyne1d = mMap.addPolyline(new PolylineOptions()
+                    .clickable(true)
+                    .add(new LatLng(51.16476175829673, 4.14110284877097),
+                            new LatLng(51.163863502158144, 4.139504604950162),
+                            new LatLng(51.16309313972857, 4.138180934842239),
+                            new LatLng(51.16257917474246, 4.137313710562012),
+                            new LatLng(51.16215866116135, 4.136771904013585),
+                            new LatLng(51.161891212585495, 4.136264966509771))
+                    .visible(false)
+            );
+            stylePolyline(polyline1a);
+            stylePolyline(polyline1b);
+            stylePolyline(polilyne1c);
+            stylePolyline(polilyne1d);
+            if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+                mMap.setMyLocationEnabled(true);
+            }
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+
+            Location myLocation = locationManager.getLastKnownLocation(locationManager
+                    .getBestProvider(criteria, false));
+            huidigeLocatie = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(huidigeLocatie, 20));
+
+        } catch (Exception Locatie){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Er is geen locatie beschikbaar");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Yes",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            builder1.setNegativeButton(
+                    "No",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
     }
+
+    private void stylePolyline(Polyline polyline) {
+        polyline.setWidth(8);
+        polyline.setColor(rgb(146,46,76));
+        polyline.setJointType(JointType.ROUND);
+    }
+
+    class firstTask extends TimerTask {
+        @Override
+        public void run() {
+
+
+            if(afstand(huidigeLocatie, new LatLng(51.17039054011921, 4.142726591135215 )) < 5) {
+            polyline1a.setVisible(false);
+            polyline1b.setVisible(true);
+            }
+
+
+
+
+        }
+    };
+    public double afstand(LatLng p1, LatLng p2){
+        double R = 6378137; // Earth’s mean radius in meter
+        double dLat = toRadians(p2.latitude - p1.latitude);
+        double dLong = toRadians(p2.latitude - p1.latitude);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(toRadians(p1.latitude)) * Math.cos(toRadians(p2.latitude)) *
+                        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        return d;
+    };
+
 }
