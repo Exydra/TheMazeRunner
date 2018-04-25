60405db8b6effeb679b5bc7dec77b804e06cd7e8
nickje10 <nicolasbaeke@hotmail.be>
2018-04-25 20:11:07 +0200

    Routes toegevoegd in spinner

--- app/src/main/java/com/example/jan_c/themazerunner/ranking.java
+++ app/src/main/java/com/example/jan_c/themazerunner/ranking.java
@@ -11,12 +11,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class ranking extends AppCompatActivity {
 
     Spinner spinner;
-    // ArrayAdapter<CharSequence> adapter;
+    ArrayList<Parcour> parcours;
+    Dictionary<Integer, Integer> parcourDictionary;
 
 
     @Override
@@ -24,17 +28,35 @@ public class ranking extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         spinner = (Spinner) findViewById(R.id.routeSpinner);
-
         List<String> list = new ArrayList<>();
-        list.add("prutser");
-        list.add("oke");
+
+
+
+        getParcoursUitlezen getParcoursUitlezen = new getParcoursUitlezen();
+        parcourDictionary = new Hashtable<>();
+        try {
+            getParcoursUitlezen.execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        parcours = getParcoursUitlezen.parcours;
+        for (Integer i = 0; i<parcours.size(); i++){
+            parcourDictionary.put(i,parcours.get(i).parcourID);
+        }
+        for (Integer i = 0; i<parcours.size();i++){
+           list.add(parcours.get(i).omschrijving);
+        }
+
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
 
                 Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is geselecteerd.",Toast.LENGTH_LONG).show();
             }
